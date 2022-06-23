21e6d670efbfa58396540f602ed1bcd25485e5fd
leo0807 <zhangjunxu3@gmail.com>
2022-06-23 23:48:57 +0800

    Update Malloc

--- malloc/main.c
+++ malloc/main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <assert.h>
 #include <stdbool.h>
+
 #define HEAP_CAP 640000
 #define HEAP_ALLOCED_CHUNKS_CAP 1024
+#define HEAP_FREED_CAP 1024
+
 typedef struct
 {
   void *start;
@@ -10,30 +14,72 @@ typedef struct
 } Heap_Chunk;
 
 char heap[HEAP_CAP] = {0};
-Heap_Chunk heap_alloced_chunks[HEAP_ALLOCED_CHUNKS_CAP] = {0};
-size_t heap_alloced_size = 0;
 size_t heap_size = 0;
+
+Heap_Chunk heap_alloced[HEAP_ALLOCED_CHUNKS_CAP] = {0};
+size_t heap_alloced_size = 0;
+
+Heap_Chunk heap_freed[HEAP_FREED_CAP] = {0};
+size_t heap_freed_size = 0;
 void *heap_alloc(size_t size)
 {
-  assert(heap_size + size <= HEAP_CAP);
-  void *result = heap_size + size; // chunk的初始位置
-  heap_size += size;               // chunk的大小
-  return result;
+  if (size > 0)
+  {
+    assert(heap_size + size <= HEAP_CAP);
+    void *result = heap_size + heap; // chunk的初始位置
+    heap_size += size;               // chunk的大小
+    const Heap_Chunk chunk = {
+        .start = result,
+        .size = size,
+    };
+    assert(heap_alloced_size < HEAP_ALLOCED_CHUNKS_CAP);
+    heap_alloced[heap_alloced_size++] = chunk;
+    return result;
+  }
+  else
+  {
+    return NULL;
+  }
 }
+void heap_dump_alloced_chunks(void)
+{
+  printf("Allocated Chunks (%zu):\n", heap_alloced_size);
+  for (size_t i = 0; i < heap_alloced_size; i++)
+  {
+    printf(" start: %p, size: %zu\n", heap_alloced[i].start, heap_alloced[i].size);
+  }
+}
+// O(Alloced)
 void heap_free(void *ptr)
 {
-  (void)ptr;
+  for (size_t i = 0; i < heap_alloced_size; i++)
+  {
+    if (heap_alloced[i].start == ptr)
+    {
+    }
+  }
 }
 void heap_collect()
 {
 }
 int main()
 {
-  char *root = heap_alloc(69);
-  for (size_t i = 0; i < 26; i++)
+  // for (size_t i = 0; i < 100; i++)
+  // {
+  //   heap_alloc(i);
+  // }
+
+  // char *root = heap_alloc(26);
+  // for (size_t i = 0; i < 26; i++)
+  // {
+  //   root[i] = i + 'A';
+  // }
+  // heap_dump_alloced_chunks();
+  // heap_free(root);
+  for (size_t i = 0; i < 5; i++)
   {
-    root[i] = i + 'A';
+    printf("%p\n", malloc(0));
   }
-  heap_free(root);
+
   return 0;
 }
